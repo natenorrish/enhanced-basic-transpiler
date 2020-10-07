a111fa57859222d9fe3d27c47b23b153df66c63d
natenorrish <42674085+natenorrish@users.noreply.github.com>
2020-10-07 18:26:24 +1100

    Update convert.js
    Added #DEFINE NAME VALUE
    
    Quick hack to convert all strings to {{string index}} to avoid overwriting variable names inside strings
    
    Remove multiple spaces in lines

--- convert.js
+++ convert.js
@@ -15,12 +15,19 @@ if (src === null || dst === null)
 var reSubName = /@([A-Z_]+[0-9_]*)(:)?/i;
 // for parsing variable names NAME$, NAME%, NAME!
 var reVarName = /(^|[^A-Z0-9_]|\s+)([A-Z_]+[0-9_]*)([%$!]{1})/ig;
+// regular expression for @DEFINE NAME VALUE
+var reDefine = /#DEFINE\s+([A-Z_]+[0-9_]*)\s+([^\n]+)/i;
+var reDefineVars = null;
+// string regular expression
+var reString = /"[^"]+"/
 
 var code = fs.readFileSync(src).toString().split('\r\n');
 var ln = 10;
 var res = [];
 var labels = {};
+var defines = {};
 var vars = {};
+var strings = [];
 var varCount = 0;
 
 // first letter of the generated var names, must be a letter
@@ -35,6 +42,7 @@ var reservedWords = [
 ];
 
 
+var match;
 for (var i=0; i<code.length; i++)
 {
 	var line = code[i].trim();
@@ -44,7 +52,43 @@ for (var i=0; i<code.length; i++)
 
 	// ignore blank lines
 	if (line == '')
+	{
+		code[i] = line;
 		continue;
+	}
+
+	// convert strings to {{123}} - a hack to ignore possible variable names
+	// inside strings. These tags will be replaced at the end with the
+	// original string.
+
+	while (match = reString.exec(line))
+	{
+		line = line.replace(match[0], '{{' + strings.length + '}}');
+		strings.push(match[0]);
+	}
+
+	match = reDefine.exec(line);
+	if (match)
+	{
+		var name = match[1]
+		var val = match[2];
+
+		defines[name] = val;
+		reDefineVars = new RegExp('(^|[^A-Z0-9_]+)(' + Object.keys(defines).join('|') + ')([^A-Z0-9_%!$]+|$)');
+
+		code[i] = '';
+		continue;
+	}
+
+	// convert define vars
+	if (reDefineVars !== null)
+	{
+		while (match = reDefineVars.exec(line))
+		{
+			line = line.replace(match[2], defines[match[2]]);
+		}
+	}
+
 
 	// convert full name variables into unique C64 2 character names
 	// to avoid confliction
@@ -52,7 +96,7 @@ for (var i=0; i<code.length; i++)
 
 	while (true)
 	{
-		var match = reVarName.exec(line);
+		match = reVarName.exec(line);
 
 		if (match)
 		{
@@ -72,10 +116,13 @@ for (var i=0; i<code.length; i++)
 	}
 
 
+	// replace line with any modifications
+	code[i] = line;
+
 	if (line == '')
 		continue;
 
-	var match = reSubName.exec(line);
+	match = reSubName.exec(line);
 
 	if (match)
 	{
@@ -93,7 +140,6 @@ ln = 10;
 for (var i=0; i<code.length; i++)
 {
 	var line = code[i].trim();
-	line = line.replace(/^\s*['].*$/, '');
 
 	if (line == '')
 		continue;
@@ -103,7 +149,7 @@ for (var i=0; i<code.length; i++)
 	{
 		if (match[2] === ':')
 		{
-			res.push((ln - 1) + ' REM '); // + '*'.repeat(match[1].length + 6));
+			res.push((ln - 1) + ' REM ');
 			line = 'REM ** ' + match[1] + ' **';
 		}
 		else
@@ -118,7 +164,16 @@ for (var i=0; i<code.length; i++)
 	ln += 10;
 }
 
+
 var str = 'PRINT CHR$($0F)\r\n' + res.join('\r\n') + '\r\nCLS\r\nRUN\r\n';
+
+// reduce multiple whitespaces to single space
+str = str.replace(/[\t ]+/g, ' ');
+
+// replace strings
+for (var i=0; i<strings.length; i++)
+	str = str.replace('{{' + i + '}}', strings[i]);
+
 fs.writeFileSync(dst, str);
 
 function getVarAlias(name, type)
