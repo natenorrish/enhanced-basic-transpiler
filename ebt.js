13947cb991b233cc1cfc6c1c2f3cf6b7af7c892a
natenorrish <42674085+natenorrish@users.noreply.github.com>
2020-10-10 01:40:29 +1100

    Update ebt.js
    Recoded to tokenize and output directly to a PRG file instead of a BASIC text file.
    Renamed to just Enhanced Basic Transpiler.
    Renamed main code file (will split out into files later and neaten code)

--- ebt.js
+++ ebt.js
@@ -1,7 +1,7 @@
 var fs = require('fs');
 var { exec } = require('child_process');
 var args = process.argv;
-var usePETSCII = false, runX16 = false, runProg = false, invalidArgs = false,
+var usePETSCII = false, runProg = false, invalidArgs = false,
     src, dst;
 
 // parse command line arguments
@@ -19,7 +19,6 @@ else
 		switch (args[i])
 		{
 			case '-p': usePETSCII = true; break;
-			case '-x': runX16 = true; break;
 			case '-r': runProg = true; break;
 			default:
 				if (args[i][0] === '-')
@@ -51,16 +50,14 @@ if (invalidArgs)
 	console.log(
 		[
 			'',
-			'X16 Enhanced Basic Transpiler',
+			'Enhanced Basic Transpiler',
 			'',
-			'Usage: convert [-OPTIONS] src.bas dst.bas',
-			'   or: node convert src.bas dst.bas',
+			'Usage: node ebt [-OPTIONS] src.bas dst.prg',
 			'',
 			'OPTIONS:',
 			'--------',
 			'',
-			'-x    Run X16 Emulator',
-			'-r    Run program (auto add CLS and RUN to the end)',
+			'-r    Run program',
 			'-p    Use PETSCII character set'
 		].join('\n')
 	);
@@ -68,29 +65,116 @@ if (invalidArgs)
 	return;
 }
 
-// regular expressions for parsing @LABEL and @LABEL:
-var reSubName = /@([A-Z_]+[0-9_]*)(:)?/i;
-// for parsing variable names NAME$, NAME%, NAME!
-var reVarName = /(^|[^A-Z0-9_]|\s+)([A-Z_]+[0-9_]*)([%$!#]{1})/ig;
-// regular expression for @DEFINE NAME VALUE
-var reDefine = /#DEFINE\s+([A-Z_]+[0-9_]*)\s+([^\n]+)/i;
-var reDefineVars = null;
-// string regular expression
-var reString = /"[^"]+"/
 
-var code = fs.readFileSync(src).toString().split('\r\n');
-var ln = 10;
-var res = [];
-var labels = {};
-var defines = {};
-var vars = {};
-var strings = [];
-var varCount = 0;
+var binaryMap = {
+	'END'     : 128,
+	'FOR'     : 129,
+	'NEXT'    : 130,
+	'DATA'    : 131,
+	'INPUT#'  : 132,
+	'INPUT'   : 133,
+	'DIM'     : 134,
+	'READ'    : 135,
+	'LET'     : 136,
+	'GOTO'    : 137,
+	'RUN'     : 138,
+	'IF'      : 139,
+	'RESTORE' : 140,
+	'GOSUB'   : 141,
+	'RETURN'  : 142,
+	'REM'     : 143,
+	'STOP'    : 144,
+	'ON'      : 145,
+	'WAIT'    : 146,
+	'LOAD'    : 147,
+	'SAVE'    : 148,
+	'VERIFY'  : 149,
+	'DEF'     : 150,
+	'POKE'    : 151,
+	'PRINT#'  : 152,
+	'PRINT'   : 153,
+	'CONT'    : 154,
+	'LIST'    : 155,
+	'CLR'     : 156,
+	'CMD'     : 157,
+	'SYS'     : 158,
+	'OPEN'    : 159,
+	'CLOSE'   : 160,
+	'GET'     : 161,
+	'NEW'     : 162,
+	'TAB('    : 163,
+	'TO'      : 164,
+	'FN'      : 165,
+	'SPC('    : 166,
+	'THEN'    : 167,
+	'NOT'     : 168,
+	'STEP'    : 169,
+	'+'       : 170,
+	'-'       : 171,
+	'*'       : 172,
+	'/'       : 173,
+	'^'       : 174,
+	'AND'     : 175,
+	'OR'      : 176,
+	'>'       : 177,
+	'='       : 178,
+	'<'       : 179,
+	'SGN'     : 180,
+	'INT'     : 181,
+	'ABS'     : 182,
+	'USR'     : 183,
+	'FRE'     : 184,
+	'POS'     : 185,
+	'SQR'     : 186,
+	'RND'     : 187,
+	'LOG'     : 188,
+	'EXP'     : 189,
+	'COS'     : 190,
+	'SIN'     : 191,
+	'TAN'     : 192,
+	'ATN'     : 193,
+	'PEEK'    : 194,
+	'LEN'     : 195,
+	'STR$'    : 196,
+	'VAL'     : 197,
+	'ASC'     : 198,
+	'CHR$'    : 199,
+	'LEFT$'   : 200,
+	'RIGHT$'  : 201,
+	'MID$'    : 202,
+	'GO'      : 203,
 
-// first letter of the generated var names, must be a letter
-var varHiChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
-// second letter can be a number
-var varLoChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+	// X16 
+	'CHAR'    : 0x8bce,
+	'CLS'     : 0x90ce,
+	'COLOR'   : 0x8dce,
+	'DOS'     : 0x81ce,
+	'FRAME'   : 0x89ce,
+	'GEOS'    : 0x83ce,
+	'JOY'     : 0x95ce,
+	'LINE'    : 0x88ce,
+	'MON'     : 0x80ce,
+	'MOUSE'   : 0x8cce,
+	'MX'      : 0x92ce,
+	'MY'      : 0x93ce,
+	'MB'      : 0x94ce,
+	'OLD'     : 0x82ce,
+	'PSET'    : 0x87ce,
+	'RECT'    : 0x8ace,
+	'RESET'   : 0x8fce,
+	'SCREEN'  : 0x86ce,
+	'VPOKE'   : 0x84ce,
+	'VPEEK'   : 0x91ce,
+	'VLOAD'   : 0x85ce
+}
+
+var reserved = Object.keys(binaryMap);
+
+// reserved words but don't have a binary representation
+var reservedRaw = [
+	'TIME',
+	'TIME$'
+];
 
 // if a generated variable name contains one of the following,
 // it'll be disgarded and another name will be generated
@@ -102,166 +186,416 @@ var reservedVars = [
 	'GET#', 'INPUT#', 'PRINT#'
 ];
 
+// first letter of the generated var names, must be a letter
+var varHiChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+// second letter can be a number
+var varLoChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
 
-var match;
-for (var i=0; i<code.length; i++)
+
+var strings = [];
+var comments = [];
+var labels = [];
+var numbers = [];
+var labelLineMap = {};
+var memLocation = 0x0801
+
+var vars = [];
+var varAliases = [];
+var varCount = 0;
+
+var lines = fs.readFileSync(src).toString().split(/[\r\n]+/);
+
+if (!usePETSCII)
+	lines.unshift('PRINT CHR$(15)');
+
+var parsed = parse(lines);
+var output = build(parsed);
+
+fs.writeFileSync(dst, output);
+
+if (runProg)
 {
-	var line = code[i].trim();
-
-	// remove Qbasic style commented lines
-	line = line.replace(/^\s*['].*$/, '');
-
-	// ignore blank lines
-	if (line == '')
-	{
-		code[i] = line;
-		continue;
-	}
-
-	// convert strings to {{123}} - a hack to ignore possible variable names
-	// inside strings. These tags will be replaced at the end with the
-	// original string.
-
-	while (match = reString.exec(line))
-	{
-		line = line.replace(match[0], '{{' + strings.length + '}}');
-		strings.push(match[0]);
-	}
-
-	match = reDefine.exec(line);
-	if (match)
-	{
-		var name = match[1]
-		var val = match[2];
-
-		defines[name] = val;
-		reDefineVars = new RegExp('(^|[^A-Z0-9_]+)(' + Object.keys(defines).join('|') + ')([^A-Z0-9_%!$#]+|$)');
-
-		code[i] = '';
-		continue;
-	}
-
-	// convert define vars
-	if (reDefineVars !== null)
-	{
-		while (match = reDefineVars.exec(line))
+	exec(
+		'x16emu -prg ' + dst + ' -run',
+		(error, stdout, stderr) =>
 		{
-			line = line.replace(match[2], defines[match[2]]);
+			
 		}
-	}
+	);
+}
 
 
-	// convert full name variables into unique C64 2 character names
-	// to avoid confliction
-	reVarName.lastIndex = 0;
+function parse(lines)
+{
+	// replace comments, strings and reserved keywords with tags {x:0}
+	// r - reserved keyword
+	// a - reserved raw
+	// v - variable
+	// l - label
+	// g - goto / label ref
+	// c - comment
+	// n - number
 
-	while (true)
+	// FIRST PASS: Find comments and strings
+
+	var match;
+	for (var i=0; i<lines.length; i++)
 	{
-		match = reVarName.exec(line);
+		var line = lines[i].trim();
 
-		if (match)
+		while (match = /['"]/.exec(line))
 		{
-			var name = match[2];
-			var type = match[3];
-			var alias = getVarAlias(name, type);
+			var start = match.index, end = -1;
 
-			if (alias !== false)
+			if (match[0] == '\'')
 			{
-				line = line.replace(new RegExp('([^A-Z0-9_]|^|\s+)' + name + '\\' + type), match[1] + alias);
-				code[i] = line;
-				reVarName.lastIndex = match.index + alias.length + match[1].length;
+				end = line.length;
+				var comment = line.substr(start + 1);
+				line = insertTag(line, 'c', comments.length, start, line.length - start);
+				comments.push(comment);
 			}
 			else
 			{
-				// reserved var name found, skip it
-				reVarName.lastIndex += name.length + type.length;
+				end = line.indexOf('"', start + 1);
+
+				if (end < 0)
+					error('Could not find end of string', i);
+
+				var str = line.substr(start + 1, end - start - 1);
+				line = insertTag(line, 's', strings.length, start, end - start +1);
+				strings.push(str);
 			}
-			
 		}
-		else
-		{
-			break;
-		}
+
+		lines[i] = line.trim();
 	}
 
-
-	// replace line with any modifications
-	code[i] = line;
-
-	if (line == '')
-		continue;
-
-	match = reSubName.exec(line);
-
-	if (match)
+	// SECOND PASS: Replace variables, labels and keywords
+	var ln = 10;
+	for (var i=0; i<lines.length; i++)
 	{
-		if (match[2] === ':')
+		var line = lines[i];
+
+		// numbers
+		while (match = /\$[A-F0-9]+/i.exec(line))
 		{
-			var label = match[1];
-			labels[label] = ln;
+			var index = numbers.indexOf(match[0]);
+			if (index < 0)
+			{
+				index = numbers.length;
+				numbers.push(match[0]);
+			}
+
+			line = insertTag(line, 'n', index, match.index, match[0].length);
 		}
+
+		while (match = /(^|[^A-Z0-9_]|\s+)([A-Z_]+[A-Z0-9_]*)(?:([%$!#:])|(\s*[#(]{1}))?/.exec(line))
+		{
+
+			// check if it's a reserved keyword
+			var checkReserved = match[0].replace(/[^A-Z0-9_(%$!#]+/g, '');
+			if (reserved.indexOf(checkReserved) > -1)
+			{
+				var before = match[1].length;
+				line = insertTag(line, 'r', binaryMap[checkReserved], match.index + before, match[0].length - before);
+			}
+			else if (reservedRaw.indexOf(checkReserved) > -1)
+			{
+				var index = reservedRaw.indexOf(checkReserved);
+				var before = match[1].length;
+				line = insertTag(line, 'a', index, match.index + before, match[0].length - before);
+			}
+			else
+			{
+				var start = match[1].trim();
+				var end = match[3] ? match[3].trim() : '';
+
+				if (start === '@')
+				{
+					var type = (match[3] === ':' ? 'l' : 'g');
+					var label = match[2].trim();
+					var index = labels.indexOf(label);
+
+					if (index < 0)
+					{
+						index = labels.length;
+						labels.push(label);
+					}
+
+					line = insertTag(line, type, index, match.index, match[0].length);
+
+					if (type === 'l')
+					{
+						labelLineMap[label] = ln;
+					}
+				}
+				else if (start === '#')
+				{
+					// skip directives for now
+					line = '';
+				}
+				else if (['', '$', '!', '#', '%'].indexOf(end) > -1)
+				{
+					// variable
+					var name = match[2].trim();
+					var type = match[3] || '';
+					var before = match[1].length;
+
+					index = getVarIndex(name, type);
+					line = insertTag(line, 'v', index, match.index + before, match[0].length - before);
+				}
+				else
+				{
+					console.log(match);
+					return;
+				}
+			}
+		}
+
+		// replace operators
+		while (match = /[+*/^<>=-]/.exec(line))
+		{
+			if (binaryMap[match[0]] === undefined)
+			{
+				console.log(match);
+				return;
+			}
+
+			line = insertTag(line, 'o', binaryMap[match[0]], match.index, 1);
+		}
+
+		line = line.trim();
+		if (line !== '')
+			ln += 10;
+
+		lines[i] = line.replace(/\s+/g, ' ');
 	}
 
-	ln += 10;
+	lines = lines.filter(l => l !== '');
+
+	// go through each line into an array of tags and characters
+	for (var i=0; i<lines.length; i++)
+	{
+		var line = lines[i];
+		var _line = [];
+		var p = 0, c, type, end, id;
+
+		while (p < line.length)
+		{
+			var c = line[p];
+
+			if (c === '{')
+			{
+				// next char 
+				p++;
+				// get type
+				type = line[p];
+				// skip type char and :
+				p += 2;
+				// find end of tag
+				end = line.indexOf('}', p);
+
+				// throw error if one isn't found - shouldn't happen
+				if (end < 0)
+					throw 'Invalid tag';
+
+				// extract the ID from the tag
+				id = line.substr(p, end - p);
+
+				// set position to end tag position
+				p = end;
+
+				_line.push([ type, id ]);
+			}
+			else
+			{
+				// not a tag so add char to array
+				_line.push(c);
+			}
+
+			// next char
+			p++;
+		}
+
+		lines[i] = _line;
+
+	}
+
+	return lines;
 }
 
-ln = 10;
-for (var i=0; i<code.length; i++)
+function build(lines)
 {
-	var line = code[i].trim();
+	var buf = Buffer.allocUnsafe(64000).fill(0), pos = 0;
+	var line, node, ln = 10;
 
-	if (line == '')
-		continue;
+	pos = writeWord(buf, pos, memLocation);
 
-	var match = reSubName.exec(line);
-	if (match)
+	for (var i=0; i<lines.length; i++)
 	{
-		if (match[2] === ':')
+		var nextLineOffsetPos = pos;
+
+		// 2 byte placeholder for next line offset
+		pos += 2;
+
+		// write line number
+		pos = writeWord(buf, pos, ln);
+
+		line = lines[i];
+		for (var j=0; j<line.length; j++)
 		{
-			res.push((ln - 1) + ' REM ');
-			line = 'REM ** ' + match[1] + ' **';
+			node = line[j];
+
+			if (typeof(node) === 'object')
+			{
+				switch (node[0])
+				{
+					case 'o':
+					case 'r':
+						var val = node[1];
+						if (val < 256) pos = writeByte(buf, pos, val);
+						else pos = writeWord(buf, pos, val);
+						break;
+
+					case 'a':
+						pos = writeRaw(buf, pos, reservedRaw[node[1]].toString());
+						break;
+
+					case 's':
+						var val = strings[node[1]];
+						pos = writeString(buf, pos, val);
+						break;
+
+					case 'l':
+						pos = writeComment(buf, pos, '*** ' + labels[node[1]] + ' ***');
+						break;
+
+					case 'g':
+						pos = writeRaw(buf, pos, labelLineMap[labels[node[1]]].toString());
+						break;
+
+					case 'v':
+						var alias = varAliases[node[1]];
+						pos = writeRaw(buf, pos, alias);
+						break;
+
+					case 'c':
+						pos = writeComment(buf, pos, comments[node[1]]);
+						break;
+
+					case 'n':
+						pos = writeRaw(buf, pos, numbers[node[1]].toString());
+						break;
+				}
+			}
+			else
+			{
+				pos = writeByte(buf, pos, node.charCodeAt(0));
+			}
 		}
-		else
-		{
-			line = line.replace(match[0], labels[match[1]]);
-		}
+
+		// back fill next line offset
+		writeWord(buf, nextLineOffsetPos, memLocation + pos - 1);
+
+		// add a zero char
+		pos++;
+
+		// increment line number
+		ln += 10;
 	}
 
+	// add 2 zero chars to end
+	pos += 2;
 
 
-	res.push(ln + ' ' + line);
-	ln += 10;
+	// test = '';
+	// for (var i=0; i<pos; i+=2)
+	// {
+	// 	test += buf[i].toString(16).padStart(2, '0') + buf[i + 1].toString(16).padStart(2, '0') + ' ';
+	// }
+
+	// console.log(test);
+
+	// buf = fs.readFileSync('TIME.PRG');
+	// test = '';
+	// for (var i=0; i<buf.length - 1; i+=2)
+	// {
+	// 	test += buf[i].toString(16).padStart(2, '0') + buf[i + 1].toString(16).padStart(2, '0') + ' ';
+	// }
+	// console.log(test);
+
+	return buf.slice(0, pos);
 }
 
+function writeByte(buf, pos, val)
+{
+	buf[pos++] = val;
+	return pos;
+}
 
-var str = (usePETSCII ? '' : '\x0F') + res.join('\r\n') + '\r\n' + (runProg ? 'CLS\r\nRUN\r\n' : '');
+function writeWord(buf, pos, val)
+{
+	var lo = val & 0xFF;
+	var hi = (val & 0xFF00) >> 8;
 
-// reduce multiple whitespaces to single space
-str = str.replace(/[\t ]+/g, ' ');
+	buf[pos++] = lo;
+	buf[pos++] = hi;
 
-// replace strings
-for (var i=0; i<strings.length; i++)
-	str = str.replace('{{' + i + '}}', strings[i]);
+	return pos;
+}
 
-fs.writeFileSync(dst, str);
+function writeString(buf, pos, val)
+{
+	buf[pos++] = 0x22;
 
-exec(
-	'x16emu -bas ' + dst + ' -keymap en-us',
-	(error, stdout, stderr) =>
-	{
+	for (var i=0; i<val.length; i++)
+		buf[pos++] = val.charCodeAt(i);
 
-	}
-);
+	buf[pos++] = 0x22;
 
+	return pos;
+}
+
+function writeRaw(buf, pos, val)
+{
+	for (var i=0; i<val.length; i++)
+		buf[pos++] = val.charCodeAt(i);
+
+	return pos;
+}
+
+function writeComment(buf, pos, val)
+{
+	buf[pos++] = 0x8F;
+	buf[pos++] = 0x20;
+	return writeRaw(buf, pos, val);
+}
+
+function insertTag(line, type, id, start, len)
+{
+	return line.substr(0, start) + '{' + type + ':' + id + '}' + line.substr(start + len);
+}
 
 function getVarAlias(name, type)
+{
+	var index = getVarIndex(name, type);
+	
+	if (index === false)
+		return false;
+
+	return varAliases[index];
+}
+
+function getVarIndex(name, type)
 {
 	var _name = name + type;
 
 	if (reservedVars.indexOf(_name) > -1)
 		return false;
 
-	if (typeof(vars[_name]) === 'undefined')
+	var index = vars.indexOf(_name);
+
+	if (index < 0)
 	{
 		var alias;
 
@@ -290,9 +624,17 @@ function getVarAlias(name, type)
 		}
 
 		if (type === '!' || type === '#') type = '';
-		vars[_name] = alias.trim() + type;
+
+		index = vars.length;
+		vars.push(_name);
+		varAliases.push(alias.trim() + type);
 		varCount++;
 	}
 
-	return vars[_name];
+	return index;
+}
+
+function error(msg, line)
+{
+	throw 'ERROR: ' + msg + ' (line: ' + (line + 1) + ')';
 }
